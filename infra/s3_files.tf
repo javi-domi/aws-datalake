def0043d0a723c3d2c43832961fe900b8cb6acfb
javi-domi <javdominguez_@outlook.com>
2022-10-18 17:12:43 -0300

    [hotfix]: fix misspelling in spark_job file name

--- infra/s3_files.tf
+++ infra/s3_files.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket_object" "spark_job" {
   bucket = aws_s3_bucket.datalake.id
   key    = "emr-code/pyspark/spark_job.py"
   acl    = "private"
-  source = "../spark_job.py"
-  etag   = filemd5("../spark_job.py")
+  source = "../etl/spark_job.py"
+  etag   = filemd5("../etl/spark_job.py")
 }
